49455a6773d1dc7e0ac260f03d998ddb2797d397
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2020-07-06 17:47:03 +0300

    fix(firebase): export missing service

--- src/shared/services/firebase/firebase.module.ts
+++ src/shared/services/firebase/firebase.module.ts
@@ -1,9 +1,10 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { FirebaseService } from './service/firebase.service';
+import { FirebaseService } from 'src/shared/services/firebase/service/firebase.service';
 
 @Module({
   imports: [ConfigModule],
   providers: [FirebaseService],
+  exports: [FirebaseService],
 })
 export class FirebaseModule {}
