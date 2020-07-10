476bd60ad7c59336b7d732be2013a250c605a20d
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2020-07-10 20:56:54 +0300

    fix(fb): fix firebase node names

--- src/shared/utils/cleaner.ts
+++ src/shared/utils/cleaner.ts
@@ -4,3 +4,10 @@ export function cleanName(name: string): string {
     .toLowerCase()
     .replace(/[: ]/g, '_');
 }
+
+export function cleanFbNodeName(name: string): string {
+  return (name || '')
+    .trim()
+    .toLowerCase()
+    .replace(/[\.\#\$\[\]]/g, '');
+}
