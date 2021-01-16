c08da0d8955d876a741a78f9206dce1ff7ec61cd
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-01-17 00:40:02 +0200

    chore: remove bin files

--- bin/rp-temp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while :
-do
- temp=$(/opt/vc/bin/vcgencmd measure_temp | cut -f1 -d\' | cut -f2 -d=)
- echo $temp
- curl -X PUT http://127.0.0.1:3000/node/rpi/cpu_temp/$temp
- sleep 30
-done
