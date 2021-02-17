c0f04d85b5bcb2d17f1cc756b5a9b10984a3e925
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-02-17 22:07:48 +0200

    chore: debug msgpack raw data

--- src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
+++ src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
@@ -7,6 +7,7 @@ export class MsgPackMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const contentType = req.header('content-type');
     if (contentType === 'application/msgpack') {
+      console.log(req.body.toString('hex'));
       req.body = MessagePack.decode(req.body);
     }
     next();
