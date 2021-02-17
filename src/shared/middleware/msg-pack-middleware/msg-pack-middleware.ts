2f97d3977e051e5a3c366467bd6b3563ed57b24d
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-02-17 22:00:34 +0200

    chore: check if msgpack has no issue like msgpack-lite

--- src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
+++ src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
@@ -1,13 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
-import * as MessagePack from 'msgpack-lite';
+import * as msgpack from 'msgpack';
 
+// import * as MessagePack from 'msgpack-lite';
 @Injectable()
 export class MsgPackMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const contentType = req.header('content-type');
     if (contentType === 'application/msgpack') {
-      req.body = MessagePack.decode(req.body);
+      // req.body = MessagePack.decode(req.body);
+      req.body = msgpack.unpack(req.body);
     }
     next();
   }
