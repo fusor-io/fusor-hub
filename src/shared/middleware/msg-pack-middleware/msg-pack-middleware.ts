2dbc4e42c1550b224f7c79f4eef26e0b66139b90
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-02-17 22:05:32 +0200

    chore: undo msgpack experiment

--- src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
+++ src/shared/middleware/msg-pack-middleware/msg-pack-middleware.ts
@@ -1,15 +1,13 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
-import * as msgpack from 'msgpack';
+import * as MessagePack from 'msgpack-lite';
 
-// import * as MessagePack from 'msgpack-lite';
 @Injectable()
 export class MsgPackMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     const contentType = req.header('content-type');
     if (contentType === 'application/msgpack') {
-      // req.body = MessagePack.decode(req.body);
-      req.body = msgpack.unpack(req.body);
+      req.body = MessagePack.decode(req.body);
     }
     next();
   }
