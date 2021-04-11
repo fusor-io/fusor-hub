c70dfd28ad217e9239bc87fc6cd57b1c55fc8f9b
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-04-11 18:21:34 +0300

    feat(cors): add cors origin for browser apps

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
-import * as hidePoweredBy from 'hide-powered-by';
+import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
+import * as hidePoweredBy from 'hide-powered-by';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(hidePoweredBy());
   app.use(bodyParser.raw({ type: 'application/msgpack' }));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: '*',
+  });
   app.enableShutdownHooks();
   await app.listen(3000);
 }
