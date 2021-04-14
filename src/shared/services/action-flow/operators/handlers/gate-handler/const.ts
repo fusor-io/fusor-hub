aac7996ec799b2a4c7a3459d60e371b6c19356d6
Giedrius Lukosevicius <g.lukosevicius@gmail.com>
2021-04-14 23:17:45 +0300

    chore: sync naming with fe admin

--- src/shared/services/action-flow/operators/handlers/gate-handler/const.ts
+++ src/shared/services/action-flow/operators/handlers/gate-handler/const.ts
@@ -1,4 +1,4 @@
-export const INPUT_VALUE = 'value';
+export const INPUT_VALUE = 'in';
 export const INPUT_GATE = 'gate';
 export const INPUT_NAMES = [INPUT_VALUE, INPUT_GATE];
-export const OUTPUT_OUT = 'value';
+export const OUTPUT_OUT = 'out';
